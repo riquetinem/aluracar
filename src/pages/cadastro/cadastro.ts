18920dbbf77733d74a9cfaddc7d8ac5e9f510a39
Henrique Shinhate Tinem <henrique714tinem@gmail.com>
2018-11-30 16:41:13 -0200

    Validação do agendamento de veiculo

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, Alert } from 'ionic-angular';
 import { Carro } from '../../modelos/carro';
+import { AgendamentosServiceProvider } from '../../providers/agendamentos-service/agendamentos-service';
+import { HomePage } from '../home/home';
+import { Agendamento } from '../../modelos/agendamento';
 
 @IonicPage()
 @Component({
@@ -17,18 +20,67 @@ export class CadastroPage {
   public email: string = '';
   public data: string = new Date().toISOString();
 
+  private _alerta: Alert;
+
   constructor(public navCtrl: NavController, 
-    public navParams: NavParams) {
+    public navParams: NavParams,
+    private _alertContrl: AlertController,
+    private _agendamentosService: AgendamentosServiceProvider) {
 
       this.carro = this.navParams.get('carroSelecionado');
       this.precoTotal = this.navParams.get('precoTotal');
   }
 
   agenda() {
-    console.log(this.nome);
-    console.log(this.endereco);
-    console.log(this.email);
-    console.log(this.data);
+    if(!this.nome || !this.endereco || !this.email){
+      this._alertContrl.create({
+        title: 'Preenchimento obrigatorio!',
+        subTitle: 'Preencha todos os campos',
+        buttons:[
+          { text: 'ok' }
+        ]
+      }).present();
+
+      return;
+    }
+
+    let agendamento: Agendamento = {
+      nomeCliente: this.nome,
+      enderecoCliente: this.endereco,
+      emailCliente: this.email,
+      modeloCarro: this.carro.nome,
+      precoTotal: this.precoTotal
+
+    };
+
+    this._alerta = this._alertContrl.create({
+      title: 'Aviso',
+      buttons: [
+        { text: 'ok',
+          handler: () => {
+            this.navCtrl.setRoot(HomePage);
+          }
+      }
+      ]
+    });
+
+    let mensagem = '';
+
+    this._agendamentosService.agenda(agendamento)
+      .finally(
+        () => {
+          this._alerta.setSubTitle(mensagem);
+          this._alerta.present();
+        }
+      )
+      .subscribe(
+      () => {
+        mensagem = 'Agendamento realizado!';
+      },
+      () => {
+        mensagem = 'Falha no Agendamento. Tente novamente mais tarde!';
+      }
+      );
   }
 
   ionViewDidLoad() {
--- src/providers/agendamentos-service/agendamentos-service.ts
+++ src/providers/agendamentos-service/agendamentos-service.ts
@@ -5,8 +5,14 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class AgendamentosServiceProvider {
 
+  private _url = 'http://localhost:8080/api';
+
   constructor(private _http: HttpClient) {
       
+  }
+
+  agenda(agendamento) {
+    return this._http.post(this._url + '/agendamento/agenda', agendamento);
   }
 
 }
